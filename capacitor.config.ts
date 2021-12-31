9cec425ceb68357419f5dfd5e3e4aa531d1000bb
Jonathan Damico <jj_damico@yahoo.com>
2021-12-30 18:58:00 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'workmail-interface',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
